512f8d2354927e13dc63d73e23bd8998a5e4e2f8
pepisg <pedro.gonzalez@eia.edu.co>
2025-01-27 16:46:02 +0000

    try to fix conflicts

--- nav2_mppi_controller/test/critics_tests.cpp
+++ nav2_mppi_controller/test/critics_tests.cpp
@@ -549,7 +549,7 @@ TEST(CriticTests, PathAlignCritic)
   data.goal_checker = &goal_checker;
 
   // Initialization testing
-
+  
   // Make sure initializes correctly
   PathAlignCritic critic;
   critic.on_configure(node, "mppi", "critic", costmap_ros, &param_handler);
@@ -563,7 +563,6 @@ TEST(CriticTests, PathAlignCritic)
   goal.position.x = 0.85;
   critic.score(data);
   EXPECT_NEAR(costs.sum(), 0.0, 1e-6);
-
   // provide state pose and path far enough to enable
   // but data furthest point reached is 0 and offset default is 20, so returns
   path.x(9) = 0.15;
